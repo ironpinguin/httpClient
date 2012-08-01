76f0945342ae07003bbb7e0155d6c3f9f66df047
Michele Catalano <michele@catalano.de>
2012-08-01 05:52:06 +0200

    Initial Commit

---
+++ IronpinguinBrowserBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Http browser bundle to make http[s] calls from a symfony project.
+ *
+ * Copyright see LICENSE file.
+ *
+ * User: michele
+ * Date: 29.07.12 04:56
+ */
+namespace Ironpinguin\BrowserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IronpinguinBrowserBundle extends Bundle
+{
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+IronpinguinBrowserClient - A simple HTTP Client
+===============================================
+
+What does it?
+-------------
+
+This Bundle brings you a simple HTTP Client with a Adapter concept for using
+different php extension (curl, httprequest) to make the http requests from your php application.
