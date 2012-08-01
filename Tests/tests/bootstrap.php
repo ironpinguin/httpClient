147a90bf881fae3ce553747c0b65db34a2977265
Michele Catalano <michele@catalano.de>
2012-08-01 05:53:04 +0200

    Testing enviroment inital setting up.

---
+++ Tests/tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Http browser bundle to make http[s] calls from a symfony project.
+ *
+ * Copyright see LICENCES file.
+ *
+ * User: michele
+ * Date: 01.08.12 05:30
+ */
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
