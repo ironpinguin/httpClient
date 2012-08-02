678e3e54e8a337a4b4454ebdeb957c9df12a8da1
Michele Catalano <michele@catalano.de>
2012-08-02 23:14:14 +0200

    Add supported HTTP Methods. First methods to handle headers and rawBody.

--- Message/Message.php
+++ Message/Message.php
@@ -12,5 +12,70 @@ namespace Ironpinguin\BrowserBundle\Message;
 abstract class Message
 {
     const METHOD_GET = "GET";
+    const METHOD_POST = "POST";
+    const METHOD_PUT = "PUT";
+    const METHOD_HEAD = "HEAD";
+    const METHOD_DELETE = "DELETE";
+    const METHOD_PATCH = "PATCH";
+
+    private $_header = array();
+
+    private $_rawBody = "";
+
+    public function getHeaders()
+    {
+        return $this->_header;
+    }
+
+    public function getHeader($name)
+    {
+        $result = null;
+        if (array_key_exists($name, $this->_header))
+        {
+            $result = $this->_header[$name];
+        }
+        return $result;
+    }
+
+    public function getHeadersToSend()
+    {
+        $headers = array();
+        foreach($this->_header as $name => $value)
+        {
+            $headers[] = "$name: $value";
+        }
+        return $headers;
+    }
+
+    public function setHeaders(array $headers)
+    {
+        $this->_header = $headers;
+    }
+
+    public function setHeader($name, $value, $overwrite = false)
+    {
+        $result = false;
+        if(!array_key_exists($name, $this->_header))
+        {
+            $this->_header[$name] = $value;
+            $result = true;
+        }
+        elseif ($overwrite)
+        {
+            $this->_header[$name] = $value;
+            $result = true;
+        }
+        return $result;
+    }
+
+    public function getRawBody()
+    {
+        return $this->_rawBody;
+    }
+
+    public function setRawBody($rawBody)
+    {
+        $this->_rawBody = $rawBody;
+    }
 
 }
