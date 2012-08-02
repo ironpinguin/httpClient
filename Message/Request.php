2f8a24391a78343a7ae6111e44b09c98b5c6c212
Michele Catalano <michele@catalano.de>
2012-08-02 23:17:23 +0200

    Add uri, baseUri and method fields with getter/setter

--- Message/Request.php
+++ Message/Request.php
@@ -10,5 +10,40 @@
 namespace Ironpinguin\BrowserBundle\Message;
 class Request extends Message
 {
+    private $_baseUri;
+
+    private $_uri;
+
+    private $_method;
+
+    public function __construct($url = null)
+    {
+        if (!is_null($url))
+        {
+            // TODO: Add uri Check.
+            $this->_baseUri = $url;
+            $this->_uri = $url;
+        }
+    }
+
+    public function setMethod($method)
+    {
+        $this->_method = $method;
+    }
+
+    public function getMethod()
+    {
+        return $this->_method;
+    }
+
+    public function setUri($uri)
+    {
+        $this->_uri = $uri;
+    }
+
+    public function getUri()
+    {
+        return $this->_uri;
+    }
 
 }
